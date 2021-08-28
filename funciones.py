8ec3c83480f70c036bc22a2b1e9fd8a0e7459925
genelopez1115 <genesislopezmendez@gmail.com>
2021-08-28 12:23:02 -0500

    pruebas

---
+++ funciones.py
@@ -0,0 +1,35 @@
+# def imprimir_mensaje():
+#     print("mensaje especial: ")
+#     print("estoy aprendiendo a usar funciones")
+
+    
+#     imprimir_mensaje()
+
+
+
+# opcion = int(input("Elige una opcion (1, 2, 3): "))
+
+# def mensajes():
+#     print ("Hola")
+#     print("Como estas")
+#     print("elegiste la opcion " + str(opcion))
+#     print("fin")
+
+# if opcion == 1:
+#     mensajes()
+# elif opcion == 2:
+#     mensajes()
+# elif opcion == 3:
+#     mensajes()
+# else:
+#     print ("escoge una opcion valida")
+
+
+# def suma(a,b):
+#     print("suma de dos numeros")
+#     resultado = a + b
+#     return resultado
+
+# sumatoria = suma(1, 4)
+# print (sumatoria)
+    
