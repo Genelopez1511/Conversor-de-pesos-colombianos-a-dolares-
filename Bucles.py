971a368855abd87ef5ffde922b12e1bd557d0944
genelopez1115 <genesislopezmendez@gmail.com>
2021-09-04 15:28:58 -0500

    actualizacion 2.0

--- Bucles.py
+++ Bucles.py
@@ -1,17 +1,52 @@
-def potencia(numero):
-    potencia = 1
+# def potencia(numero):
+#     potencia = 1
 
 
-    while (potencia <= 10):
-        result = numero ** potencia
-        print("potencia de {} elevado a la es {} es {}".format(numero, potencia, result))
-        potencia += 1
+#     while (potencia <= 10):
+#         result = numero ** potencia
+#         print("potencia de {} elevado a la es {} es {}".format(numero, potencia, result))
+#         potencia += 1
+
+
+# def run():
+#     numero = int(input("Escribe el numero el cual quieres averiguarle la portencia"))
+#     potencia(numero)
+
+
+# if __name__=="__main__":
+#     run() 
+
+
+# contador = 0
+# print('2 elevado a ' + str(contador) + ' es igual a: ' + str(2**contador))
+
+# contador = 1
+# print('2 elevado a ' + str(contador) + ' es igual a: ' + str(2**contador))
+
+# contador = 2
+# print('2 elevado a ' + str(contador) + ' es igual a: ' + str(2**contador))
+
+# contador = 3
+# print('2 elevado a ' + str(contador) + ' es igual a: ' + str(2**contador))
+
+# contador = 4
+# print('2 elevado a ' + str(contador) + ' es igual a: ' + str(2**contador))
+
+# contador = 5
+# print('2 elevado a ' + str(contador) + ' es igual a: ' + str(2**contador))
+
+# Este codigo repetido se puede resolver con el cilo While (Mientras) de la siguiente forma:
 
 
 def run():
-    numero = int(input("Escribe el numero el cual quieres averiguarle la portencia"))
-    potencia(numero)
+    LIMITE = 1000000
 
+    contador = 0
+    potencia_2 = 2**contador
+    while potencia_2 < LIMITE:
+        print('2 elevado a ' + str(contador) + ' es igual a: ' + str(potencia_2))
+        contador = contador + 1
+        potencia_2 = 2**contador
 
 if __name__=="__main__":
-    run() 
+    run()
